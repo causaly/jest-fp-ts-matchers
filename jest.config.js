e693b6051762fb4200f84d2bac6f6e79a9de66ca
Dimitrios C. Michalakos <dimitris@jmike.gr>
2023-04-21 16:36:48 +0300

    fix: switch to pure JS jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
+module.exports = {
   preset: 'ts-jest',
   collectCoverageFrom: ['lib/**/*.{js,ts}'],
   testRegex: './lib/.*\\.(test|spec)\\.(js|ts)$',
 };
-
-export default config;
